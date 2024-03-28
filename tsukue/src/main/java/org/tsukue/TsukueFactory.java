171863d8e1e11ab3d28da31dc8800c83c74dfdfe
aki matsuno <matsuaki4@gmail.com>
2024-03-29 01:38:02 +0900

    deskとtableそれぞれでworking typeとstanding typeが作れることを確認

--- tsukue/src/main/java/org/tsukue/TsukueFactory.java
+++ tsukue/src/main/java/org/tsukue/TsukueFactory.java
@@ -15,4 +15,20 @@ public class TsukueFactory {
         Building building = new WorkingBuilding(BuildingType.TABLE, pillar, topBoard, drawer);
         return new Table(building) {};
     }
+
+    public static Tsukue createWorkingDesk() {
+        Pillar pillar = new Pillar(new Wood(), new Square(), 10.7, 20.8, 30.9, 1);
+        TopBoard topBoard = new TopBoard(new Wood(), new Circle(), 40.7, 50.8, 60.9);
+        Drawer drawer = new Drawer(new Wood(), new Square(), 70.7, 80.8, 90.9, 10);
+        Building building = new WorkingBuilding(BuildingType.DESK, pillar, topBoard, drawer);
+        return new Desk(building) {};
+    }
+
+    public static Tsukue createStandingTable(){
+        Pillar pillar = new Pillar(new Wood(), new Square(), 10.7, 20.8, 30.9, 1);
+        TopBoard topBoard = new TopBoard(new Wood(), new Circle(), 40.7, 50.8, 60.9);
+        Drawer drawer = new Drawer(new Wood(), new Square(), 70.7, 80.8, 90.9, 10);
+        Building building = new StandBuilding(BuildingType.TABLE, pillar, topBoard);
+        return new Table(building) {};
+    }
 }
--- tsukue/src/test/java/org/tsukue/TsukueTest.java
+++ tsukue/src/test/java/org/tsukue/TsukueTest.java
@@ -12,9 +12,17 @@ class TsukueTest {
         void StandingDeskが構築されること(){
             assertEquals("DESK Building! pillar=Pillar{material=Wood, shape=Square, width=10.7, height=20.8, depth=30.9, number=1}, topBoard=TopBoard{material=Wood, shape=Circle, width=40.7, height=50.8, depth=60.9}", TsukueFactory.createStandingDesk().build());
         }
+
+        @Test
+        void WorkingDeskが構築されること(){
+            assertEquals("DESK Building! pillar=Pillar{material=Wood, shape=Square, width=10.7, height=20.8, depth=30.9, number=1}, topBoard=TopBoard{material=Wood, shape=Circle, width=40.7, height=50.8, depth=60.9}, drawer=Drawer{material=Wood, shape=Square, width=70.7, height=80.8, depth=90.9, num=10}", TsukueFactory.createWorkingDesk().build());
+        }
     }
     @Nested
     class TableTest{
+        void StandingTableが構築されること(){
+            assertEquals("DESK Building! pillar=Pillar{material=Wood, shape=Square, width=10.7, height=20.8, depth=30.9, number=1}, topBoard=TopBoard{material=Wood, shape=Circle, width=40.7, height=50.8, depth=60.9}", TsukueFactory.createWorkingTable().build());
+        }
         @Test
         void WorkingTableが構築されること(){
             assertEquals("TABLE Building! pillar=Pillar{material=Wood, shape=Square, width=10.7, height=20.8, depth=30.9, number=1}, topBoard=TopBoard{material=Wood, shape=Circle, width=40.7, height=50.8, depth=60.9}, drawer=Drawer{material=Wood, shape=Square, width=70.7, height=80.8, depth=90.9, num=10}", TsukueFactory.createWorkingTable().build());

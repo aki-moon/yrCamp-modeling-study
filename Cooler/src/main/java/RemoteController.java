32aa5b21c1acc9aa9b2ec58d8cdc5d572f70e393
aki matsuno <matsuaki4@gmail.com>
2024-04-24 20:25:02 +0900

    不要なフィールド変数の削除

--- Cooler/src/main/java/RemoteController.java
+++ Cooler/src/main/java/RemoteController.java
@@ -3,11 +3,9 @@ public class RemoteController {
     private Condition condition = Condition.OFF;
     // FIXME 暫定的に室内の空気を固定
     private Air indoorAir = new Air(24.0, 50.0, Direction.NORTH);
-    private HygrothermalControl hygrothermalControl = new HeatingHygrothermalControl();
 
     public void start(HygrothermalControl hygrothermalControl, double expectTemperature, double expectHumidity, Direction expectDirection) {
         this.condition = Condition.ON;
-        this.hygrothermalControl = hygrothermalControl;
         Air expectAir = new Air(expectTemperature, expectHumidity, expectDirection);
         this.indoorAir = hygrothermalControl.control(indoorAir, expectAir);
     }

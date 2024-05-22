84e407d289fc7061dd0c1960e8ceaa31e8afecec
aki matsuno <matsuaki4@gmail.com>
2024-05-22 16:05:23 +0900

    gitignoreの設定

--- automaticgate_ts/src/index.ts
+++ automaticgate_ts/src/index.ts
@@ -1,2 +1,286 @@
-const message: string = "Hello, World!";
-console.log(message);
+abstract class ElectronicPass {
+    constructor(
+      private _balance: Number,
+      private _autoCharge: boolean,
+      private _route: Route,
+      private _expirationDate: Date,
+      private _entrainStation: Station
+    ) {}
+  
+    get balance(): Number {
+      return this._balance;
+    }
+  
+    get autoCharge(): boolean {
+      return this._autoCharge;
+    }
+  
+    get route(): Route {
+      return this._route;
+    }
+  
+    get expirationDate(): Date {
+      return this._expirationDate;
+    }
+  
+    get entrainStation(): Station {
+      return this._entrainStation;
+    }
+  
+    abstract charge(): void;
+    abstract debit(): void;
+  }
+  
+  abstract class Ticket {
+    constructor(
+      private _side: Side,
+      private _route: Route,
+      private _expirationDate: Date,
+      private _entrainStation: Station
+    ) {}
+  
+    get side(): Side {
+      return this._side;
+    }
+  
+    get route(): Route {
+      return this._route;
+    }
+  
+    get expirationDate(): Date {
+      return this._expirationDate;
+    }
+  
+    get entrainStation(): Station {
+      return this._entrainStation;
+    }
+  
+    abstract arrangeSide(): void;
+  }
+  
+  enum Side {
+    FRONT,
+    BACK,
+  }
+  
+  class NormalTicket extends Ticket {
+    arrangeSide(): void {
+      // Implementation
+    }
+  }
+  
+  class RoundTripTicket extends Ticket {
+    arrangeSide(): void {
+      // Implementation
+    }
+  }
+  
+  class CouponTicket extends Ticket {
+    arrangeSide(): void {
+      // Implementation
+    }
+  }
+  
+  class CommuterTicket extends Ticket {
+    arrangeSide(): void {
+      // Implementation
+    }
+  }
+  
+  class AutomaticGate {
+    constructor(
+      private passReader: PassReader,
+      private ticketReader: TicketReader,
+      private automaticGateStation: Station,
+      private door: Door,
+      private electronicPass: ElectronicPass,
+      private ticket: Ticket
+    ) {}
+  
+    start(): void {
+      // Implementation
+    }
+  
+    stop(): void {
+      // Implementation
+    }
+  
+    open(): void {
+      // Implementation
+    }
+  
+    close(): void {
+      // Implementation
+    }
+  
+    notifyAlert(): void {
+      // Implementation
+    }
+  
+    coveredCommuterPass(): boolean {
+      // Implementation
+      return false;
+    }
+  
+    coveredTicket(): boolean {
+      // Implementation
+      return false;
+    }
+  
+    fare(): Number {
+      // Implementation
+      return 0;
+    }
+  
+    debit(): void {
+      // Implementation
+    }
+  
+    punchTicket(): void {
+      // Implementation
+    }
+  
+    printTicket(): void {
+      // Implementation
+    }
+  }
+  
+  class TicketReader {
+    constructor(private status: OpenStatus) {}
+  
+    isTicket(): boolean {
+      // Implementation
+      return false;
+    }
+  
+    start(): void {
+      // Implementation
+    }
+  
+    stop(): void {
+      // Implementation
+    }
+  }
+  
+  class PassReader {
+    constructor(private status: OpenStatus) {}
+  
+    isElectronicPass(): boolean {
+      // Implementation
+      return false;
+    }
+  
+    start(): void {
+      // Implementation
+    }
+  
+    stop(): void {
+      // Implementation
+    }
+  }
+  
+  class Door {
+    constructor(private status: PassageStatus) {}
+  
+    open(): void {
+      // Implementation
+    }
+  
+    close(): void {
+      // Implementation
+    }
+  
+    checkPerson(): void {
+      // Implementation
+    }
+  }
+  
+  enum OpenStatus {
+    OPEN,
+    CLOSE,
+  }
+  
+  enum PassageStatus {
+    PASSABLE,
+    IMPASSABLE,
+  }
+  
+  class Route {
+    constructor(
+      private _startStation: Station,
+      private _exitStation: Station
+    ) {}
+  
+    get startStation(): Station {
+      return this._startStation;
+    }
+  
+    get exitStation(): Station {
+      return this._exitStation;
+    }
+  
+    isCovered(): boolean {
+      // Implementation
+      return false;
+    }
+  }
+  
+  class Station {
+    constructor(
+      private _name: string,
+      private _code: number,
+      private _lineList: Line[]
+    ) {}
+  
+    get name(): string {
+      return this._name;
+    }
+  
+    get code(): number {
+      return this._code;
+    }
+  
+    get lineList(): Line[] {
+      return this._lineList;
+    }
+  }
+  
+  class Line {
+    constructor(private _name: string, private _code: number) {}
+  
+    get name(): string {
+      return this._name;
+    }
+  
+    get code(): number {
+      return this._code;
+    }
+  }
+  
+  class Suica extends ElectronicPass {
+    charge(): void {
+      // Implementation
+    }
+  
+    debit(): void {
+      // Implementation
+    }
+  }
+  
+  class Pasmo extends ElectronicPass {
+    charge(): void {
+      // Implementation
+    }
+  
+    debit(): void {
+      // Implementation
+    }
+  }
+  
+  class CreditCard {
+    // Implementation
+  }
+  
+  class AppleWatch {
+    // Implementation
+  }

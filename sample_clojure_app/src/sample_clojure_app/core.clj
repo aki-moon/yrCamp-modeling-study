1863a1f42a6fc88c7169ddf88c531fe234fae612
aki matsuno <matsuaki4@gmail.com>
2024-08-07 23:20:55 +0900

    clojureで3章のサンプルプログラムの実行

--- sample_clojure_app/src/sample_clojure_app/core.clj
+++ sample_clojure_app/src/sample_clojure_app/core.clj
@@ -1,8 +1,20 @@
 (ns sample-clojure-app.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn fibs-work [n i fs]
+  (if (= i n)
+    fs
+    (fibs-work n (inc i) (conj fs (apply + (take-last 2 fs))))))
+(defn fibs [n]
+  (cond
+    (< n 1) []
+    (= n 1) [1]
+    :else (fibs-work n 2 [1 1])))
 
-(+ 1 1)
+(defn fibre [n]
+  (cond
+    (< n 1) nil
+    (<= n 2) 1
+    :else (+ (fibre (dec n)) (fibre (- n 2)))))
+
+(defn fibsre [n]
+  (map fibre (range 1 (inc n))))
--- sample_clojure_app/test/sample_clojure_app/core_test.clj
+++ sample_clojure_app/test/sample_clojure_app/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [sample-clojure-app.core :refer :all]))
 
-(deftest a-test
+(deftest fibs-work-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= fibs 15 [1 1 2 3 5 8 13 21 34 55 89 144 233 377 610]))))

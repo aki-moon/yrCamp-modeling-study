7f773333a0926102517826b16dfcbbe4446f67e9
aki matsuno <matsuaki4@gmail.com>
2024-08-07 18:23:49 +0900

    initial commit

---
+++ sample_clojure_app/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sample_clojure_app
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ sample_clojure_app/src/sample_clojure_app/core.clj
@@ -0,0 +1,8 @@
+(ns sample-clojure-app.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(+ 1 1)
